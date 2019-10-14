7331756abeed5b3c76b00062b84543a09d650e69
danielshenkutie <50990501+danielshenkutie@users.noreply.github.com>
2019-10-14 20:33:59 +0200

    add preference and bookings

---
+++ src/model/preferences.ts
@@ -0,0 +1,26 @@
+export default {
+  id: "oebjergkjbergkjjb",
+  interval: 15,
+  createTime: new Date(),
+  fromTodayBoundary: 10, // you should not be able to pick a time futher a head than x days into the future
+  monday: {
+    startTimeInMinutes: 600,
+    endTimeInMinutes: 1200
+  },
+  tuesday: {
+    startTimeInMinutes: 600,
+    endTimeInMinutes: 960
+  },
+  wednesday: {
+    startTimeInMinutes: 600,
+    endTimeInMinutes: 1200
+  },
+  thursday: {
+    startTimeInMinutes: 600,
+    endTimeInMinutes: 960
+  },
+  friday: {
+    startTimeInMinutes: 600,
+    endTimeInMinutes: 1200
+  }
+};
