e292cb6c5d94b4d36d5c1e41dffd60a216f1a6e1
danielshenkutie <50990501+danielshenkutie@users.noreply.github.com>
2019-10-17 11:17:44 +0200

    add types

--- src/types/types.ts
+++ src/types/types.ts
@@ -8,3 +8,23 @@ export interface ITimeSlot {
   startTime: Date;
   endTime: Date;
 }
+
+export interface IDateTimeWindow {
+  date: string;
+  bookingStartTime: Date;
+  bookingEndTime: Date;
+}
+
+export interface IDateWithTimeSlots extends IDateTimeWindow {
+  timeSlot: ITimeSlot[];
+}
+
+export interface IBookedTimeSlot {
+  id: number;
+  createTime: Date;
+  startTime: Date;
+  endTime: Date;
+}
+export interface IDateTimeWindowGrouped {
+  [index: string]: IDateTimeWindow[];
+}
