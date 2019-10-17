26a16d13a60fbcd9a26c769bf70e022f74abca37
danielshenkutie <50990501+danielshenkutie@users.noreply.github.com>
2019-10-17 11:18:06 +0200

    use service

--- src/resolvers/resolver.ts
+++ src/resolvers/resolver.ts
@@ -1,11 +1,18 @@
 import {
   getDateRange,
   filterDatesByDay,
-  availableDateTimeWindow,
-  generateTimeSlots
+  mapAvailableDateToTimeWindows,
+  generateTimeSlotsForDates,
+  groupBookedTimeSlotsByDate,
+  checkIfTimeSlotsOverLap,
+  removeBookedTimeSlots
 } from "../dateUtils/dateUtils";
 import preferences from "../model/preferences";
+import BookingService from "../services/BookingService";
+import { IBookedTimeSlot } from "../types/types";
 import _ from "lodash";
+import UserService from "../services/UserService";
+import { SchedulingService } from "../services/SchedulingServic";
 
 const weekDays = [
   "monday",
@@ -19,31 +26,14 @@ const weekDays = [
 export default {
   Query: {
     testMessage: (parent: any, { input }: any, ctx: any, info: any): string => {
-      const dateRange = getDateRange(new Date(), 10);
-      const preferredDays = _.intersection(Object.keys(preferences), weekDays);
-
-      const pickedTimeWindow: { [index: string]: any } = _.pick(
-        preferences,
-        weekDays
+      const bookingService = new BookingService();
+      const userService = new UserService();
+      const schedulingService = new SchedulingService(
+        userService,
+        bookingService
       );
-      const daysWithStartAndEndTime = preferredDays.map(day => ({
-        day: day,
-        ...pickedTimeWindow[day]
-      }));
-      console.log(daysWithStartAndEndTime);
-      const filtered = filterDatesByDay(dateRange, preferredDays);
-      const windowPerDate = availableDateTimeWindow(
-        daysWithStartAndEndTime,
-        filtered
-      );
-      console.log(windowPerDate);
-      if (windowPerDate[0] !== null) {
-        generateTimeSlots(
-          windowPerDate[0].bookingStartTime,
-          windowPerDate[0].bookingEndTime,
-          15
-        );
-      }
+      const openTimeSlots = schedulingService.getOpenTimeSlots();
+      openTimeSlots.map(slot => console.log(slot));
 
       return "Hello World!";
     }
