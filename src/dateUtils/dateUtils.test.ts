ae5e80bccfc46635697692ec59f184a264692e6c
danielshenkutie <50990501+danielshenkutie@users.noreply.github.com>
2019-10-17 11:16:36 +0200

    more test

--- src/dateUtils/dateUtils.test.ts
+++ src/dateUtils/dateUtils.test.ts
@@ -1,9 +1,31 @@
 import {
   getDateRange,
   filterDatesByDay,
-  availableDateTimeWindow,
-  generateTimeSlots
+  mapAvailableDateToTimeWindows,
+  generateTimeSlots,
+  checkIfTimeSlotsOverLap
 } from "../dateUtils/dateUtils";
+import { preferredTimeWindow, ITimeSlot } from "../types/types";
+import preferences from "../model/preferences";
+import moment from "moment";
+
+const dateMonday = new Date("2019/10/14"); // Monday Oct 14 2019
+const dateTuesday = new Date("2019/10/15"); // Tuesday Oct 15 2019
+const dateWednesday = new Date("2019/10/16"); // Wednesday Oct 16 2019
+const dateThursday = new Date("2019/10/17"); // Thursday Oct 17 2019
+const dateFriday = new Date("2019/10/18"); // Friday Oct 18 2019
+const dateSaturday = new Date("2019/10/19"); // Saturday Oct 19 2019
+const dateSunday = new Date("2019/10/20"); // Sunday Oct 20 2019
+
+const dates = [
+  dateMonday,
+  dateTuesday,
+  dateWednesday,
+  dateThursday,
+  dateFriday,
+  dateSaturday,
+  dateSunday
+];
 
 test("generate date Range", () => {
   const now = new Date("2019/10/15");
@@ -22,23 +44,6 @@ test("generate date Range", () => {
 });
 
 test("Filter dates based on the day of the week", () => {
-  const dateMonday = new Date("2019/10/14"); // Monday Oct 14 2019
-  const dateTuesday = new Date("2019/10/15"); // Tuesday Oct 15 2019
-  const dateWednesday = new Date("2019/10/16"); // Wednesday Oct 16 2019
-  const dateThursday = new Date("2019/10/17"); // Thursday Oct 17 2019
-  const dateFriday = new Date("2019/10/18"); // Friday Oct 18 2019
-  const dateSaturday = new Date("2019/10/19"); // Saturday Oct 19 2019
-  const dateSunday = new Date("2019/10/20"); // Sunday Oct 20 2019
-
-  const dates = [
-    dateMonday,
-    dateTuesday,
-    dateWednesday,
-    dateThursday,
-    dateFriday,
-    dateSaturday,
-    dateSunday
-  ];
   const days = ["monday", "tuesday", "wednesday", "thursday", "friday"];
   const datesFiltered = filterDatesByDay(dates, days);
   expect(datesFiltered.length).toBe(5);
@@ -50,4 +55,156 @@ test("Filter dates based on the day of the week", () => {
   );
   expect(indexMonday).toBe(0);
   expect(indexSunday).toBe(-1);
+});
+
+describe("available dates with time window", () => {
+  const mondayPref: preferredTimeWindow = {
+    day: "monday",
+    startTimeInMinutes: 600,
+    endTimeInMinutes: 1200
+  };
+  const wednesdayPref: preferredTimeWindow = {
+    day: "wednesday",
+    startTimeInMinutes: 600,
+    endTimeInMinutes: 960
+  };
+
+  const preference = [mondayPref, wednesdayPref];
+
+  test("available dates will be given according to the weekdays preference", () => {
+    const dateMonday = new Date("Mon, 14 October 2019 13:50:00");
+    const dateWednesday = new Date("Wed, 16 October 2019 13:30:00");
+
+    const availableDate = [dateMonday, dateWednesday];
+    const dateWithTimeWindows = mapAvailableDateToTimeWindows(
+      preference,
+      availableDate
+    );
+    expect(dateWithTimeWindows.length).toBe(2);
+    expect(dateWithTimeWindows[0]).toEqual({
+      date: "2019:10:14",
+      bookingStartTime: moment(dateMonday, "YYYY:MM:DD")
+        .minutes(mondayPref.startTimeInMinutes)
+        .toDate(),
+      bookingEndTime: moment(dateMonday, "YYYY:MM:DD")
+        .minutes(mondayPref.endTimeInMinutes)
+        .toDate()
+    });
+
+    expect(dateWithTimeWindows[1]).toEqual({
+      date: "2019:10:16",
+      bookingStartTime: moment(dateWednesday, "YYYY:MM:DD")
+        .minutes(wednesdayPref.startTimeInMinutes)
+        .toDate(),
+      bookingEndTime: moment(dateWednesday, "YYYY:MM:DD")
+        .minutes(wednesdayPref.endTimeInMinutes)
+        .toDate()
+    });
+  });
+
+  test("available booking start time and booking end time should be with in the same date", () => {
+    const dateMonday = new Date("Mon, 14 October 2019 13:50:00");
+    const dateWednesday = new Date("Wed, 16 October 2019 13:30:00");
+
+    const availableDate = [dateMonday, dateWednesday];
+    const dateWithTimeWindows = mapAvailableDateToTimeWindows(
+      preference,
+      availableDate
+    );
+    expect(dateWithTimeWindows.length).toBe(2);
+    expect(
+      dateWithTimeWindows[0].bookingEndTime.getDate() ===
+        dateWithTimeWindows[0].bookingStartTime.getDate()
+    ).toBe(true);
+
+    expect(
+      dateWithTimeWindows[1].bookingEndTime.getDate() ===
+        dateWithTimeWindows[1].bookingStartTime.getDate()
+    ).toBe(true);
+  });
+
+  test("if the available dates don't fall on the preferred weekday returns empty array ", () => {
+    const availableDate = [dateTuesday, dateThursday];
+    const dateWithTimeWindows = mapAvailableDateToTimeWindows(
+      preference,
+      availableDate
+    );
+    expect(dateWithTimeWindows.length).toBe(0);
+  });
+});
+
+describe("generateTimeSlots", () => {
+  test("generates time slots with correct interval", () => {
+    const startTime = new Date();
+    let endTime = new Date(startTime);
+    endTime.setMinutes(endTime.getMinutes() + 60);
+    console.log("endTime", endTime);
+
+    const intervalMinutes = 15;
+    const timeSlots = generateTimeSlots(startTime, endTime, intervalMinutes);
+
+    expect(timeSlots.length).toBe(4);
+    expect(
+      timeSlots[0].endTime.getTime() - timeSlots[0].startTime.getTime()
+    ).toBe(intervalMinutes * 60 * 1000);
+  });
+
+  test("time slots lie with in the startTime and end Time", () => {
+    const startTime = new Date();
+    let endTime = new Date(startTime);
+    endTime.setMinutes(endTime.getMinutes() + 45);
+
+    const intervalMinutes = 15;
+    const timeSlots = generateTimeSlots(startTime, endTime, intervalMinutes);
+    expect(timeSlots.length).toBe(3);
+    const firstTimeSlot = timeSlots[0];
+    const latTimeSlot = timeSlots[2];
+    expect(
+      firstTimeSlot.startTime.getTime() === startTime.getTime()
+    ).toBeTruthy();
+    expect(latTimeSlot.endTime.getTime() === endTime.getTime()).toBeTruthy();
+  });
+});
+
+describe("checkIfTimeSlotsOverLap", () => {
+  const bookedSlot: ITimeSlot = {
+    startTime: new Date("Wed, 16 October 2019 13:30:00"),
+    endTime: new Date("Wed, 16 October 2019 13:50:00")
+  };
+
+  it("when the time slots  do not not over lapp", () => {
+    const generatedTimeSlot: ITimeSlot = {
+      startTime: new Date("Wed, 16 October 2019 13:51:00"),
+      endTime: new Date("Wed, 16 October 2019 13:55:00")
+    };
+    const isOverLap = checkIfTimeSlotsOverLap(bookedSlot, generatedTimeSlot);
+    expect(isOverLap).toBe(false);
+  });
+
+  it("when the start and end time of the second time slot are with in the bound of the first time slot", () => {
+    const generatedTimeSlot: ITimeSlot = {
+      startTime: new Date("Wed, 16 October 2019 13:35:00"),
+      endTime: new Date("Wed, 16 October 2019 13:45:00")
+    };
+    const isOverLap = checkIfTimeSlotsOverLap(bookedSlot, generatedTimeSlot);
+    expect(isOverLap).toBe(true);
+  });
+
+  it("When the start time of the second time slot lies with in the first time slot", () => {
+    const generatedTimeSlot: ITimeSlot = {
+      startTime: new Date("Wed, 16 October 2019 13:36:00"),
+      endTime: new Date("Wed, 16 October 2019 14:00:00")
+    };
+    const isOverLap = checkIfTimeSlotsOverLap(bookedSlot, generatedTimeSlot);
+    expect(isOverLap).toBe(true);
+  });
+
+  it("When the end time of the second time slot lies with in the first time slot", () => {
+    const generatedTimeSlot: ITimeSlot = {
+      startTime: new Date("Wed, 16 October 2019 13:20:00"),
+      endTime: new Date("Wed, 16 October 2019 13:40:00")
+    };
+    const isOverLap = checkIfTimeSlotsOverLap(bookedSlot, generatedTimeSlot);
+    expect(isOverLap).toBe(true);
+  });
 });
